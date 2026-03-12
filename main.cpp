10b2df8a7e18be907efc304246db5c1e7f12778a
zainab427-maker <zainab.altarouti.427@gmail.com>
2026-03-12 03:27:25 +0300

    Update and rename README.md to main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // معلوماتd الشخصية
+    string name = "Zainab"; 
+    string major = "Cyber Security / Maker";
+    string goal = "Learning GitHub and C++ basics!";
+
+    cout << "========================================" << endl;
+    cout << "           WELCOME TO MY PROFILE        " << endl;
+    cout << "========================================" << endl;
+
+    cout << "My Name is: " << Zainab << endl;
+    cout << "My Field:   " << Cyber Security<< endl;
+    cout << "My Goal:    " << Learning Github and C++ basics << endl;
+
+    cout << "----------------------------------------" << endl;
+    cout << "Nice to meet you!" << endl;
+    cout << "========================================" << endl;
+
+    return 0;
+}
